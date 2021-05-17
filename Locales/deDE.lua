09f3928f4f9f6b38f92543a6539eced7d2f30641
davidcraig <davidcraig1986@gmail.com>
2021-05-17 18:47:46 +0100

    Remove oil (wod currency).

--- Locales/deDE.lua
+++ Locales/deDE.lua
@@ -90,39 +90,15 @@ L["CFGPAGE_PROFILES"] = "Profile"
 L["CFGPAGE_SECTIONS"] = "Bereiche"
 L["CFGTXT_DELETECHAR"] = "Wenn ein Charakter gelöscht wird, werden auch alle seine gesammelten Daten gelöscht."
 L["CFGTXT_IGNOREDCHARS"] = "Ignorierte Charaktere werden komplett ignoriert. D.h. es werden keinerlei Daten erhoben, sie werden nicht im Tooltip erscheinen und sie werden zu keinen Berechnungen herangezogen. "
-L["NAME_AF_ARAKKOA"] = "Arakkoa"
-L["NAME_AF_DRAENEI"] = "Draeneiisch"
-L["NAME_AF_DRAENOR_CLANS"] = "Klans von Draenor"
-L["NAME_AF_DWARF"] = "Zwergisch"
-L["NAME_AF_FOSSIL"] = "Fossil"
-L["NAME_AF_MANTID"] = "Mantis"
-L["NAME_AF_MOGU"] = "Mogu"
-L["NAME_AF_NERUBIAN"] = "Nerubisch"
-L["NAME_AF_NIGHTELF"] = "Nachtelfisch"
-L["NAME_AF_OGRE"] = "Oger"
-L["NAME_AF_ORC"] = "Orcisch"
-L["NAME_AF_OTHER"] = "Andere"
-L["NAME_AF_PANDAREN"] = "Pandaren"
-L["NAME_AF_TOLVIR"] = "Tol'vir"
-L["NAME_AF_TROLL"] = "Trollisch"
-L["NAME_AF_VRYKULL"] = "Vrykul"
-L["NAME_ANCIENT_MANA"] = "Uraltes Mana"
-L["NAME_APEXIS_CRYSTAL"] = "Apexiskristall"
 L["NAME_ARTIFACT_FRAGMENT"] = "Artefaktfragment"
 L["NAME_BLACK_IRON_FRAGMENT"] = "Schwarzeisenfragment"
 L["NAME_BLOODYCOIN"] = "Blutige Münze"
 L["NAME_CHAMPIONSEAL"] = "Siegel des Champions"
 L["NAME_CHEFSAWARD"] = "Kochauszeichnung"
 L["NAME_CONQUESTPOINTS"] = "Eroberungspunkte"
---[[Translation missing --]]
---[[ L["NAME_CURIOUS_COINS"] = ""--]] 
 L["NAME_DALJCTOKEN"] = "Juwelierssymbol von Dalaran"
 L["NAME_DARKMOONPRIZETICKET"] = "Gewinnlos des Dunkelmond-Jahrmarkts"
 L["NAME_DINGY_IRON_COINS"] = "Schäbige Eisenmünzen"
---[[Translation missing --]]
---[[ L["NAME_ECHOES_OF_BATTLE"] = ""--]] 
---[[Translation missing --]]
---[[ L["NAME_ECHOES_OF_DOMINATION"] = ""--]] 
 L["NAME_ELDERCHARMOFGOODFORTUNE"] = "Ehrwürdiges Amulett des Glücks"
 L["NAME_EPICUREANAWARD"] = "Feinschmeckerpreis"
 L["NAME_ESSENCEDEATHWING"] = "Essenz des verderbten Todesschwinge"
@@ -131,20 +107,13 @@ L["NAME_HONORPOINTS"] = "Ehrenpunkte"
 L["NAME_ILLJCTOKEN"] = "Symbol des erhabenen Juweliers"
 L["NAME_IRONPAWTOKEN"] = "Eisentatzmarke"
 L["NAME_JUSTICEPOINTS"] = "Gerechtigkeitspunkte"
---[[Translation missing --]]
---[[ L["NAME_LEGIONFALL_WAR_SUPPLIES"] = ""--]] 
 L["NAME_LESSERGOODFORTUNE"] = "Geringes Amulett des Glücks"
 L["NAME_MARKOFTHEWORLDTREE"] = "Abzeichen des Weltenbaums"
 L["NAME_MOGORUNEOFFATE"] = "Schicksalsrune der Mogu"
 L["NAME_MONEY"] = "Gold"
 L["NAME_MOTEDARKNESS"] = "Partikel der Dunkelheit"
---[[Translation missing --]]
---[[ L["NAME_NETHERSHARD"] = ""--]] 
 L["NAME_NO"] = "Nein"
-L["NAME_OIL"] = "Öl"
 L["NAME_ORDER_RESOURCES"] = "Ordensressourcen"
---[[Translation missing --]]
---[[ L["NAME_SEAL_OF_BROKEN_FATE"] = ""--]] 
 L["NAME_SEAL_OF_INEVITABLE_FATE"] = "Siegel des unausweichlichen Schicksals"
 L["NAME_SEAL_OF_TEMPERED_FATE"] = "Siegel des geschmiedeten Schicksals"
 L["NAME_SECRETDRAENORALCHEMY"] = "Alchemiegeheimnis von Draenor"
@@ -152,22 +121,12 @@ L["NAME_SECRETDRAENORBLACKSMITHING"] = "Schmiedegeheimnis von Draenor"
 L["NAME_SECRETDRAENORJEWELCRAFTING"] = "Geheimnisse des Draenorjuwelenschleifens"
 L["NAME_SECRETDRAENORLEATHERWORKING"] = "Lederverarbeitungsgeheimnis von Draenor"
 L["NAME_SECRETDRAENORTAILORING"] = "Geheimnisse des Draenorschneiderns"
---[[Translation missing --]]
---[[ L["NAME_SHADOWY_COINS"] = ""--]] 
---[[Translation missing --]]
---[[ L["NAME_SIGHTLESS_EYE"] = ""--]] 
 L["NAME_TIMELESSCOIN"] = "Zeitlose Münze"
 L["NAME_TIMEWARPED_BADGE"] = "Zeitverzerrtes Abzeichen"
---[[Translation missing --]]
---[[ L["NAME_TIMEWORN_ARTIFACT"] = ""--]] 
 L["NAME_TOLBARADCOMMENDATION"] = "Belobigungsabzeichen von Tol Barad"
 L["NAME_VALOR"] = "Tapferkeitspunkte"
 L["NAME_VALORPOINTS"] = "Tapferkeitspunkte"
---[[Translation missing --]]
---[[ L["NAME_VEILED_ARGUNITE"] = ""--]] 
 L["NAME_WARFORGEDSEAL"] = "Kriegsemblem"
---[[Translation missing --]]
---[[ L["NAME_WRITHING_ESSENCE"] = ""--]] 
 L["NAME_YES"] = "Ja"
 L["NAME_ZENJCTOKEN"] = "Zenmarke des Juwelenschmieds"
 
